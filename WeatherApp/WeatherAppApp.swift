24bf8b3159053a4e0db0d02d3bdfe9884d50f6dd
rodi0nn <catalintoderascu14@gmail.com>
2023-03-30 13:25:22 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Cătălin-Iulian Toderașcu-Buia on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
